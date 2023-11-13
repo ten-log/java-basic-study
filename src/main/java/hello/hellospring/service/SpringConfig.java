bd3b5035ea334172176175fbe8049f3a1c82c09d
ten-log <qlqjs674@naver.com>
2023-11-13 20:45:48 +0900

    Add: spring annotation 기능 사용하여 di

---
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,19 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+}
