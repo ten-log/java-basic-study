974f1ea9d06536e9a2cebcc1d7bf09bca51c55a8
ten-log <qlqjs674@naver.com>
2023-11-14 12:19:02 +0900

    Add: spring의 component를 java코드로 직접 등록

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -5,15 +5,16 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//componentScan을 service와repository가아닌 직접설정
 @Configuration
 public class SpringConfig {
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+//
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
 }
