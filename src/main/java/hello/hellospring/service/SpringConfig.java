40dcdcf706a42159dccee4fc0aa9314eb99a5bf6
ten-log <qlqjs674@naver.com>
2023-11-15 13:46:05 +0900

    FIx: jdbc template에서 jap를 사용한 repository로 변경

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,9 +1,9 @@
 package hello.hellospring.service;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,15 +15,19 @@ import javax.sql.DataSource;
 public class SpringConfig {
 
     //Spring이 dataSource를 관리할수있게끔 주입하는 부분
-    private DataSource dataSource;
+//    private final DataSource dataSource;
+    private final EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource){
-        this.dataSource = dataSource;
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+//        this.dataSource = dataSource;
+        this.em = em;
     }
+
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRapository(em);
+//        return new JdbcTemplateMemberRepository(dataSource);
 //        return new JdbcMemberRepository(dataSource);
     }
 
